8164eef930800ca28a88a715970d526994e12cfd
HardCand01 <dibe.zack@gmail.com>
2014-07-15 22:35:25 +0200

    add: find known networks

--- hook-me/hookme.sh
+++ hook-me/hookme.sh
@@ -4,6 +4,8 @@ SRC_PATH=$(pwd)
 UTILS_PATH="$SRC_PATH/src/utils/"
 NET_PATH="/sys/class/net/"
 
+KNOWN_FILE="$SRC_PATH/known_networks.tmp"
+
 # Load all files ending with $PATTERN in $L_PATH
 function load_items () {
     local PATTERN="$1"
@@ -37,3 +39,11 @@ function check_status() {
 
 check_status $WLANS
 check_status $ETH
+
+KNOWN_NETW=$(cat /etc/network/interfaces | grep "conf" | cut -d'/' -f4)
+
+for NETWORK in $KNOWN_NETW ; do
+    cat "/etc/wpa_supplicant/$NETWORK" 2>/dev/null | grep -E $'[\t ]+ssid' | cut -d '=' -f2 | sed 's/"//g' >> $KNOWN_FILE
+done
+
+rm -f $KNOWN_FILE
